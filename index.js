0fc74f1c9b131256e6fcee423ddabc1d43797d7d
Ana Beatriz <159872976+A5L10@users.noreply.github.com>
2024-07-02 14:18:10 +0000

    Criação de endpoint e desafio /oi

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+//Desafio - criar endpoint /oi que exibe "Olá Mundo"
+app.get('/oi', function (rq, res){
+  res.send("Olá Mundo")
+})
+
 app.listen(3000)
